a35fbb420996dc1a670ef4a4e9f397801ce938c0
Anna <michalowska.aneczka@gmail.com>
2022-12-01 19:36:34 +0100

    commit_2

--- src/index.js
+++ src/index.js
@@ -5,15 +5,43 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-const getUrl = name =>
-  `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+const inputCountry = document.getElementById('search-box');
+const listCountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
 
-const result = document.querySelector('#result');
 
-const resetResult = () => {
-  // yuck, don't do that xD
+
+const resetMarkup = () => {
   result.innerHTML = '';
 };
+
+const fetchCountries = name => {
+  const parsedName = name.trim();
+  if (parsedName.length === 0) return;
+
+  const url = getUrl(parsedName);
+  return fetch(url)
+    .then(res => {
+      if (!res.ok) throw new Error('No countries for such query!');
+
+      return res.json();
+    })
+    .then(countries => {
+      console.log(countries);
+      if (countries.length > 10)
+        return displayAlert('Too many countries found. Be more specific!');
+      if (countries.length === 1) return displayCountryCard(countries[0]);
+      return displayCountriesList(countries);
+    })
+    .catch(error => {
+      console.error(error);
+      displayAlert(error.message, 'error');
+    });
+};
+
+
+
+
 
 const displayAlert = (msg, mode = 'info') => {
   const toast = document.createElement('div');
